f480a692a527c5ea371f05be71a6b8a7f4d20ae3
Smertin Nikita <smertin-nikita@mail.ru>
2021-02-05 22:25:35 +0500

    Added modules people, salary. Get date

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-# Press the green button in the gutter to run the script.
+import datetime
+
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+
+def print_datetime():
+    print(datetime.date.today())
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_datetime()
+    calculate_salary()
+    get_employees()
+
